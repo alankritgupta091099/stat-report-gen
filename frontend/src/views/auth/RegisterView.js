604918dc97788479601143f479e424b4dfadcd70
alankritgupta091099 <alankritgupta091099@gmail.com>
2020-09-20 17:04:37 +0530

    user register form complete

--- frontend/src/views/auth/RegisterView.js
+++ frontend/src/views/auth/RegisterView.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import {
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  FormHelperText,
-  Link,
-  TextField,
-  Typography,
-  makeStyles
-} from '@material-ui/core';
+import { Box , Button , Checkbox , Container, FormHelperText , Grid , Link , TextField , Typography , makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
-    height: '100%',
+    height: 'auto',
     paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(4),
+  },
+  formStyle:{
+    border: '2px solid '+theme.palette.primary.main,
+    borderRadius: '15px'
   }
 }));
 
 const RegisterView = () => {
   const classes = useStyles();
   const navigate = useNavigate();
+
+  const phoneRegExp = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
+
+  const handleSubmit = () => {
+    console.log("object")
+    navigate('/app/dashboard', { replace: true });
+  }
 
   return (
     <Page
@@ -39,12 +40,15 @@ const RegisterView = () => {
         height="100%"
         justifyContent="center"
       >
-        <Container maxWidth="sm">
-          <Formik
+        <Container maxWidth="md" className={classes.formStyle}>
+          <Formik            
             initialValues={{
               email: '',
               firstName: '',
               lastName: '',
+              orgName:'',
+              orgPosition:'',
+              mobNumber:'',
               password: '',
               policy: false
             }}
@@ -53,147 +57,153 @@ const RegisterView = () => {
                 email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
                 firstName: Yup.string().max(255).required('First name is required'),
                 lastName: Yup.string().max(255).required('Last name is required'),
+                orgName: Yup.string().max(255).required('Organisation name is required'),
+                orgPosition: Yup.string().max(255).required('Position in organisation is required'),
+                mobNumber: Yup.string().required().matches(phoneRegExp, 'Phone number is not valid'),
                 password: Yup.string().max(255).required('password is required'),
                 policy: Yup.boolean().oneOf([true], 'This field must be checked')
               })
             }
-            onSubmit={() => {
-              navigate('/app/dashboard', { replace: true });
-            }}
           >
             {({
               errors,
               handleBlur,
               handleChange,
-              handleSubmit,
+              //handleSubmit,
               isSubmitting,
               touched,
               values
             }) => (
-              <form onSubmit={handleSubmit}>
-                <Box mb={3}>
-                  <Typography
-                    color="textPrimary"
-                    variant="h2"
-                  >
-                    Create new account
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    gutterBottom
-                    variant="body2"
-                  >
-                    Use your email to create new account
-                  </Typography>
-                </Box>
-                <TextField
-                  error={Boolean(touched.firstName && errors.firstName)}
-                  fullWidth
-                  helperText={touched.firstName && errors.firstName}
-                  label="First name"
-                  margin="normal"
-                  name="firstName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.firstName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.lastName && errors.lastName)}
-                  fullWidth
-                  helperText={touched.lastName && errors.lastName}
-                  label="Last name"
-                  margin="normal"
-                  name="lastName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.lastName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.email && errors.email)}
-                  fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
-                  margin="normal"
-                  name="email"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="email"
-                  value={values.email}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.password && errors.password)}
-                  fullWidth
-                  helperText={touched.password && errors.password}
-                  label="Password"
-                  margin="normal"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                <Box
-                  alignItems="center"
-                  display="flex"
-                  ml={-1}
-                >
-                  <Checkbox
-                    checked={values.policy}
-                    name="policy"
-                    onChange={handleChange}
-                  />
-                  <Typography
-                    color="textSecondary"
-                    variant="body1"
-                  >
-                    I have read the
-                    {' '}
-                    <Link
-                      color="primary"
-                      component={RouterLink}
-                      to="#"
-                      underline="always"
-                      variant="h6"
+              <form onSubmit={handleSubmit} style={{padding:'1rem'}}>
+                <Grid container spacing={1}>
+                  <Grid item xs={12}>
+                    <Typography
+                      color="textPrimary"
+                      variant="h2"
                     >
-                      Terms and Conditions
-                    </Link>
-                  </Typography>
-                </Box>
-                {Boolean(touched.policy && errors.policy) && (
-                  <FormHelperText error>
-                    {errors.policy}
-                  </FormHelperText>
-                )}
-                <Box my={2}>
-                  <Button
-                    color="primary"
-                    disabled={isSubmitting}
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign up now
-                  </Button>
-                </Box>
-                <Typography
-                  color="textSecondary"
-                  variant="body1"
-                >
-                  Have an account?
-                  {' '}
-                  <Link
-                    component={RouterLink}
-                    to="/login"
-                    variant="h6"
-                  >
-                    Sign in
-                  </Link>
-                </Typography>
+                      Create new account
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      gutterBottom
+                      variant="body2"
+                    >
+                      Fill the following details to create a new account
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      error={Boolean(touched.firstName && errors.firstName)}
+                      fullWidth
+                      helperText={touched.firstName && errors.firstName}
+                      label="First name"
+                      margin="normal"
+                      name="firstName"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.firstName}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      error={Boolean(touched.lastName && errors.lastName)}
+                      fullWidth
+                      helperText={touched.lastName && errors.lastName}
+                      label="Last name"
+                      margin="normal"
+                      name="lastName"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.lastName}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      error={Boolean(touched.orgName && errors.orgName)}
+                      fullWidth
+                      helperText={touched.orgName && errors.orgName}
+                      label="Organisation Name"
+                      margin="normal"
+                      name="orgName"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.orgName}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      error={Boolean(touched.orgPosition && errors.orgPosition)}
+                      fullWidth
+                      helperText={touched.orgPosition && errors.orgPosition}
+                      label="Position in Organisation"
+                      margin="normal"
+                      name="orgPosition"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.orgPosition}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      error={Boolean(touched.email && errors.email)}
+                      fullWidth
+                      helperText={touched.email && errors.email}
+                      label="Email Address"
+                      margin="normal"
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="email"
+                      value={values.email}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>{/*Mobile number validation including country code*/}
+                    <TextField
+                      error={Boolean(touched.mobNumber && errors.mobNumber)} 
+                      fullWidth
+                      helperText={touched.mobNumber && errors.mobNumber}
+                      label="Mobile Number"
+                      margin="normal"
+                      name="mobNumber"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.mobNumber}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>{/*Add Auto generate password*/} {/*Forget password section*/}
+                    <TextField
+                      error={Boolean(touched.password && errors.password)}
+                      fullWidth
+                      helperText={touched.password && errors.password}
+                      label="Password"
+                      margin="normal"
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="password"
+                      value={values.password}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      color="primary"
+                      disabled={isSubmitting}
+                      fullWidth
+                      size="large"
+                      type="submit"
+                      variant="contained"
+                    >
+                      Create New Account
+                    </Button>
+                  </Grid>
+                </Grid>
               </form>
             )}
           </Formik>
