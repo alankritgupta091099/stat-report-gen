476b70330513d9acf80dce50a3c4aba3857b4a5b
alankritgupta091099 <alankritgupta091099@gmail.com>
2020-10-26 20:29:13 +0530

    upload file using button added + file validation added

--- frontend/src/views/fileDropZone/FileDropView.js
+++ frontend/src/views/fileDropZone/FileDropView.js
@@ -1,5 +1,5 @@
-import React,{ useState } from 'react';
-import { Box, Container, Card, CardContent, Typography , Grid , List , ListItem , ListItemText , Button , Snackbar , Dialog , Slide , AppBar , IconButton , Toolbar , Backdrop , CircularProgress } from '@material-ui/core';
+import React,{ useState , useEffect , useRef } from 'react';
+import { Box, Container, Card, CardContent, Typography , Grid , List , ListItem , ListItemText , Button , Snackbar , Dialog , Slide , AppBar , IconButton , Toolbar , Backdrop , CircularProgress , Fab } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import { useDropzone } from 'react-dropzone';
@@ -21,6 +21,13 @@ const useStyles = makeStyles((theme) => ({
     zIndex: theme.zIndex.drawer + 1,
     color: '#fff',
   },
+  input: {
+    display: 'none',
+  },
+  box: {
+    border: "1px "+theme.palette.primary.main+ " solid",
+    borderRadius: "5px"
+  }
 }));
 
 const getColor = (props) => {
@@ -82,56 +89,151 @@ function FileDropView(props) {
   const [table, setTable] = useState(false)
   const [finalList, setFinalList] = useState([]);
   const navigate = useNavigate();
+  const [enableFileSelection, setenableFileSelection] = useState(true);
+  const [sev, setsev] = useState("success");
+  const [files, setfiles] = useState({
+    name:"",
+    size:""
+  })
+  var listStore = useRef(store.getState().auth.list)
 
-  const files = acceptedFiles.map(file => {  
-    ExcelRenderer(file, (err, resp) => {
-      if(err){
-        console.log(err);            
-      }
-      else{
-        const {rows , cols} = resp;     
-        if( cols.length == 1 ){
-          setTable(true)
-          var linksKiList = [];
-          rows.slice(1).forEach(item => {
-            linksKiList.push(item[0])
-          });
-          setFinalList(linksKiList)
-          store.dispatch({
-              type:SET_LIST,
-              payload:linksKiList
-          })
-        } else {
-          //error handling
+  // useEffect(() => {
+  //   listStore.current=store.getState().auth.list;
+  //   if(listStore.current.length>0){
+  //     setsev('error')
+  //     setnotification(listStore.current.length)
+  //     setNotificationOpen(true)
+  //     console.log("object")
+  //   }
+  // }, [store.getState().auth.list])
+
+  // var files = ""
+
+  // files = acceptedFiles.map(file => {  
+  //   console.log(file)
+  //   var reader = new FileReader();
+  //   ExcelRenderer(file, (err, resp) => {
+  //     if(err){
+  //       console.log(err);            
+  //     }
+  //     else{
+  //       const {rows , cols} = resp;
+  //       if( cols.length == 1 ){
+  //         setTable(true)
+  //         var linksKiList = [];
+  //         rows.slice(1).forEach(item => {
+  //           linksKiList.push(item[0])
+  //         });
+  //         setFinalList(linksKiList)
+  //         store.dispatch({  
+  //             type:SET_LIST,
+  //             payload:linksKiList
+  //         })
+  //       } else {
+  //         //error handling
+  //       }
+  //     }
+  //   });     
+  //   return (
+  //     <li key={file.path}>
+  //       {file.path} - {file.size} bytes
+  //     </li>
+  //   )}
+  // );
+
+  // Create a reference to the hidden file input element
+  const hiddenFileInput = useRef(null);
+  
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file 
+  const handleChange = event => {
+      var firstElement=event.target.files[0];
+      console.log(firstElement);
+      if(firstElement.type=="text/csv" || firstElement.type=="application/vnd.oasis.opendocument.spreadsheet" || firstElement.type=="application/vnd.ms-excel" || firstElement.type=="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
+        setfiles({
+          name:firstElement.name,
+          size:firstElement.size
+        })
+        console.log(firstElement);
+        ExcelRenderer(firstElement, (err, resp) => {
+        if(err){
+          setsev("error")
+          setnotification("Something went wrong")
+          setNotificationOpen(true)
+          console.log(err);            
         }
-      }
-    });     
-    return (
-      <li key={file.path}>
-        {file.path} - {file.size} bytes
-      </li>
-    )}
-  );
+        else{
+          const {rows , cols} = resp;
+          console.log(cols);
+          if( cols.length == 1 ){
+            setTable(true)
+            var linksKiList = [];
+            rows.slice(1).forEach(item => {
+              linksKiList.push(item[0])
+            });
+            setFinalList(linksKiList)
+            store.dispatch({  
+                type:SET_LIST,
+                payload:linksKiList
+            })
+            setsev("success")
+            setnotification("Number of links in the list: "+linksKiList.length)
+            setNotificationOpen(true)
+          } else {
+            setsev("error")
+            setnotification("More that one number of column received")
+            setNotificationOpen(true)
+            setTable(false)
+          }
+        }
+      });    
+    } else {
+      setsev("error")
+      setnotification("Unacceptable file format")
+      setNotificationOpen(true)
+    } 
+  };
 
   return (
     <Page title="Generate Report" >
       <Container>
         <Box mt={3}>
-        <Snackbar open={notificationOpen} autoHideDuration={4000} onClose={()=>setNotificationOpen(false)}>
-          <Alert onClose={()=>setNotificationOpen(false)} elevation={6} variant="filled" severity="success">
+        <Snackbar open={notificationOpen} autoHideDuration={3000} onClose={()=>setNotificationOpen(false)}>
+          <Alert onClose={()=>setNotificationOpen(false)} elevation={6} variant="filled" severity={sev}>
             {notification}
           </Alert>
         </Snackbar>
           <Card>
             <CardContent>
               <Typography gutterBottom variant="h1">Generate Report</Typography>
-              <div className="container">
-                <DropBox {...getRootProps({ className: 'dropzone', isDragActive, isDragAccept, isDragReject })}>{/* Later add Hover styling and others as well */}
+              {/* <div className="container">
+                <DropBox {...getRootProps({ className: 'dropzone', isDragActive, isDragAccept, isDragReject })}>
                   <input {...getInputProps()} />
                   <h3>Drag 'n' drop some files here, or click on this section to upload files</h3><br/>
                   <h4>Accepted formats xlsx, xls, csv, odf</h4>
                 </DropBox>
-              </div>
+              </div> */}
+              <input
+                accept="text/csv , application/vnd.oasis.opendocument.spreadsheet , application/vnd.ms-excel , application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                className={classes.input}
+                ref={hiddenFileInput}
+                onChange={handleChange}
+                type="file"
+              />
+              <label htmlFor="contained-button-file">
+                <Typography variant="h6">
+                  Select File: 
+                  &nbsp;&nbsp;&nbsp;
+                  <Button variant="outlined" color="primary" component="span" onClick={handleClick}>
+                    UPLOAD
+                  </Button>
+                </Typography>
+              </label>
               <br/>
               <br/>
               <Grid container>
@@ -139,11 +241,11 @@ function FileDropView(props) {
                   {
                     table ? <>
                               <Typography variant="h2">Selected File: 
-                                <Typography variant="subtitle1" color="textSecondary">{files}</Typography>
+                                <Typography variant="subtitle1" color="textSecondary">{files.name} - {files.size} bytes</Typography>
                               </Typography>
                               <br/>
                               <Button variant="contained" color="primary" disableElevation onClick={()=>navigate('/app/report-gen/format')}>
-                                Select Formating Option
+                                Formating Option
                               </Button>
                             </>
                           : "" 
@@ -157,21 +259,21 @@ function FileDropView(props) {
                     <List>
                       <ListItem>
                         <ListItemText
-                          primary='1. Drag n drop the file in the section or click on the section to upload the file'
+                          primary='1. Click on "Upload" to upload the file'
                         />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText>
+                          2. Make sure that the file has single column only and value of First Row as "Links" <i><a href="./sample.xlsx" download>Sample File</a></i>
+                        </ListItemText>
                       </ListItem>
                       <ListItem>
                         <ListItemText
-                          primary='2. Make sure that the file has single column only and value of Row 1 "Links"'
+                          primary='3. Click on "Formatting Option" button'
                         />
                       </ListItem>
-                      <ListItem>
-                        <ListItemText
-                          primary='3. Click on "Generate" button'
-                        />
-                      </ListItem>
-                      <ListItem>
-                        <ListItemText primary='Note: Make sure that file is only of formats xlsx, xls, csv, odf'/>
+                      <ListItem className={classes.box}>
+                        <ListItemText><i>Note: Make sure that file is of <b>xlsx / xls / csv / ods</b> format only.</i></ListItemText>
                       </ListItem>
                     </List>
                   </Typography>
