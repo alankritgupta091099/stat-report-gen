76914d5d4d7b43493523a3b1b5f858123606bb47
Alankrit Gupta <alankritgupta@pop-os.localdomain>
2020-12-09 14:47:51 +0530

    production url updated

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -1,4 +1,4 @@
-const prod = 'http://localhost:8081';
+const prod = 'https://get-measurements.media';
 const dev = 'http://localhost:8080';
 
 export const API_URL = process.env.NODE_ENV === 'development' ? dev : prod;
