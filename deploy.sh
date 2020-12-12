6b6db0a9ae5ea34d5b0b1a27d3f9c6d159aa86a1
Alankrit Gupta <alankritgupta@pop-os.localdomain>
2020-12-12 18:43:02 +0530

    website header changed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-#!/bin/sh     
 git pull origin master
 cd backend
 npm install
+sudo pm2 start index.js -i max --watch
 cd ..
 cd frontend
 npm install
 npm run build
 cd ..
 sudo systemctl restart nginx
-sudo pm2 restart all
