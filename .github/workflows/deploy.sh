212a749972c34616892d170c8b7b3efe4a2f2ee3
Alankrit Gupta <alankritgupta@pop-os.localdomain>
2020-12-13 13:41:47 +0530

    auto deploy test-2

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,10 +1,10 @@
 git pull origin master
 cd backend
-npm install
+sudo npm install
 sudo pm2 start index.js -i max --watch
 cd ..
 cd frontend
-npm install
-npm run build
+sudo npm install
+sudo npm run build
 cd ..
 sudo systemctl restart nginx
